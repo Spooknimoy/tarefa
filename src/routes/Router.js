70b71df223bc803bdfac5e3bded8e3f6a2ee9823
Spooknimoy <luanpablo465@gmail.com>
2023-11-06 00:49:30 -0300

    aysync

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from '@expo/vector-icons/Ionicons';
